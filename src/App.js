33a3a252cd42d7f960926b625c9d30fe45ff58cf
joaocorner <joaoeduardocorner@gmail.com>
2022-07-19 23:42:07 -0300

    formatação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './app.css';
-import {
-  useState
-} from 'react';
+import { useState } from 'react';
 
 export default function App() {
+
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
 
@@ -26,42 +25,32 @@ export default function App() {
     }
   }
 
-  return ( <
-    div className = "app" >
-    <
-    h1 > Calculadora IMC < /h1> <
-    span > Preencha os campos abaixo para calcular seu IMC < /span>
+  return (<div className="app">
+    <h1> Calculadora IMC </h1>
+    <span> Preencha os campos abaixo para calcular seu IMC </span>
 
-    <
-    div className = "area-input" >
-    <
-    input type = "text"
-    placeholder = "Peso em kg Ex: 90"
-    value = {
-      peso
-    }
-    onChange = {
-      (e) => setPeso(e.target.value)
-    }
-    /> <
-    input type = "text"
-    placeholder = "Altura em cm Ex: 180"
-    value = {
-      altura
-    }
-    onChange = {
-      (e) => setAltura(e.target.value)
-    }
-    /> <
-    button onClick = {
-      calcularIMC
-    } >
-    Calcular <
-    /button> < /
-    div > <
-    h2 > {
-      mensagem
-    } < /h2>  < /
-    div >
+    <div className="area-input">
+
+      <input
+        type="text"
+        placeholder="Peso em kg Ex: 90"
+        value={peso}
+        onChange={(e) => setPeso(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Altura em cm Ex: 180"
+        value={altura}
+        onChange={(e) => setAltura(e.target.value)}
+      />
+
+      <button onClick={calcularIMC}>
+        Calcular
+      </button>
+
+    </div >
+    <h2>{mensagem}</h2>
+  </div >
   )
 }
